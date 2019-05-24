663dae3a778035556a11528396f546896202638d
Ivan Franchin <ivan.franchin@takeaway.com>
2019-05-24 23:24:36 +0200

    add/fix pagination in swagger

--- jobs-api/src/main/java/com/mycompany/jobsapi/config/SwaggerConfig.java
+++ jobs-api/src/main/java/com/mycompany/jobsapi/config/SwaggerConfig.java
@@ -1,12 +1,30 @@
 package com.mycompany.jobsapi.config;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+import java.lang.reflect.Type;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.fasterxml.classmate.TypeResolver;
+import com.google.common.collect.Lists;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 
+import springfox.documentation.builders.AlternateTypeBuilder;
+import springfox.documentation.builders.AlternateTypePropertyBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRule;
+import springfox.documentation.schema.AlternateTypeRuleConvention;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
@@ -17,16 +35,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.security.Principal;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import com.google.common.base.Predicate;
-import com.google.common.collect.Lists;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -77,5 +85,39 @@ public class SwaggerConfig {
     }
 
     private static final String API_KEY_NAME = "JWT_TOKEN";
+
+    // --
+    // The bean below is needed for fixing Pageable in Swagger. Otherwise, the arguments page, size and sort won't be shown.
+
+    @Bean
+    public AlternateTypeRuleConvention pageableConvention(final TypeResolver resolver) {
+        return new AlternateTypeRuleConvention() {
+            @Override
+            public int getOrder() {
+                return Ordered.LOWEST_PRECEDENCE;
+            }
+
+            @Override
+            public List<AlternateTypeRule> rules() {
+                return Arrays.asList(newRule(resolver.resolve(Pageable.class), resolver.resolve(pageableMixin())));
+            }
+        };
+    }
+
+    private Type pageableMixin() {
+        return new AlternateTypeBuilder()
+                .fullyQualifiedClassName(String.format("%s.generated.%s", Pageable.class.getPackage().getName(), Pageable.class.getSimpleName()))
+                .withProperties(Arrays.asList(property(Integer.class, "page"), property(Integer.class, "size"), property(String.class, "sort")))
+                .build();
+    }
+
+    private AlternateTypePropertyBuilder property(Class<?> type, String name) {
+        return new AlternateTypePropertyBuilder()
+                .withName(name)
+                .withType(type)
+                .withCanRead(true)
+                .withCanWrite(true);
+    }
+    // --
 
 }
