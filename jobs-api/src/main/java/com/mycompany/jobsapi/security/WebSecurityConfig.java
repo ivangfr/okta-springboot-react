007f2c76e4bc86c22810790388547ebdf51c5710
Ivan Franchin <ivan.franchin@takeaway.com>
2022-04-02 18:12:44 +0200

    replace Configuration annotation by EnableWebSecurity in WebSecurityConfig

--- jobs-api/src/main/java/com/mycompany/jobsapi/security/WebSecurityConfig.java
+++ jobs-api/src/main/java/com/mycompany/jobsapi/security/WebSecurityConfig.java
@@ -1,11 +1,11 @@
 package com.mycompany.jobsapi.security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
