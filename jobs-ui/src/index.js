189602d546f2181bb25f52e67b676c163388386a
Ivan Franchin <ivan.franchin@takeaway.com>
2022-06-07 00:10:08 +0200

    update jobs-ui based on a new app created using npx create-react-app

--- jobs-ui/src/index.js
+++ jobs-ui/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    // <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    // </React.StrictMode>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
