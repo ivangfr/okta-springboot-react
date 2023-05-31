61538c32873b446e8366b74d9f08321da2211770
Ivan Franchin <ivan.franchin@takeaway.com>
2023-05-31 09:55:41 -0300

    Project upgrade - upgrade to spring-boot 3.1.0; - upgrade to springdoc-openapi 2.1.0; - upgrade to elasticsearch docker image 8.7.1; - refactor JobController, CorsConfig, WebSecurityConfig and ES config in application.yml.

--- jobs-api/src/main/java/com/ivanfranchin/jobsapi/security/WebSecurityConfig.java
+++ jobs-api/src/main/java/com/ivanfranchin/jobsapi/security/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package com.ivanfranchin.jobsapi.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,18 +15,20 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/callback/token").permitAll()
-                .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/jobs/newest").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/jobs", "/api/jobs/**").hasAnyAuthority(JOBS_CUSTOMER, JOBS_STAFF)
-                .requestMatchers(HttpMethod.PUT, "/api/jobs/search").hasAnyAuthority(JOBS_CUSTOMER, JOBS_STAFF)
-                .requestMatchers("/api/jobs", "/api/jobs/**").hasAuthority(JOBS_STAFF)
-                .requestMatchers("/", "/error", "/csrf", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated();
-        http.oauth2ResourceServer().jwt();
-        http.cors().and().csrf().disable();
-        return http.build();
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/callback/token").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/jobs/newest").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/jobs", "/api/jobs/**").hasAnyAuthority(JOBS_CUSTOMER, JOBS_STAFF)
+                        .requestMatchers(HttpMethod.PUT, "/api/jobs/search").hasAnyAuthority(JOBS_CUSTOMER, JOBS_STAFF)
+                        .requestMatchers("/api/jobs", "/api/jobs/**").hasAuthority(JOBS_STAFF)
+                        .requestMatchers("/", "/error", "/csrf", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
 
     private static final String JOBS_STAFF = "JOBS_STAFF";
